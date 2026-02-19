d981388872cb6378416122c5d8343583af21d095
KHATRIRAMESH <khatriramesh972@gmail.com>
2026-02-19 22:36:34 +0545

    separate repo initialized for simplicity

---
+++ src/app.js
@@ -0,0 +1,22 @@
+
+import "dotenv/config.js";
+import express from 'express';
+import postsRoute from "./routes/posts.route.js"
+import authRoutes from "./routes/auth.route.js"
+
+const app = express();
+
+app.use(express.json()); // Middleware to parse JSON bodies
+
+
+
+app.get("/", (req, res) => {
+    res.send("Blog Post API - Welcome!");
+});
+
+app.use("/api/post", postsRoute);
+app.use("/api/auth", authRoutes);
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+});
