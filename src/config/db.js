bf76a76ca52d0857f0881d8cc059f66b0f171268
KHATRIRAMESH <khatriramesh972@gmail.com>
2026-02-25 11:27:41 +0545

    undici removed from db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,6 @@
 // Drizzle example with the Neon serverless driver
-import { setGlobalDispatcher, Agent } from "undici";
-setGlobalDispatcher(new Agent({ connect: { family: 4 } }));
+// import { setGlobalDispatcher, Agent } from "undici";
+// setGlobalDispatcher(new Agent({ connect: { family: 4 } }));
 import "dotenv/config.js";
 import { neonConfig, neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
