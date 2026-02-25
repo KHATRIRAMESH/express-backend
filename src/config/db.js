c0b42f458a1fe1f791239ce91309aa76b17eb2ad
KHATRIRAMESH <khatriramesh972@gmail.com>
2026-02-25 12:08:43 +0545

    schema added to the config file

--- src/config/db.js
+++ src/config/db.js
@@ -2,11 +2,12 @@
 // import { setGlobalDispatcher, Agent } from "undici";
 // setGlobalDispatcher(new Agent({ connect: { family: 4 } }));
 import "dotenv/config.js";
-import { neonConfig, neon } from '@neondatabase/serverless';
+import { neon, neonConfig } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
+import * as  schema from "../models/schema.js";
 
-neonConfig.fetchConnectionCache = true;
+
 
 const sql = neon(process.env.DATABASE_URL);
 
-export const db = drizzle(sql);
+export const db = drizzle(sql, { schema });
