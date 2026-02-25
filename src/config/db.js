a0a7285e7bbe3d1f6f7664404860dceaa7857e60
KHATRIRAMESH <khatriramesh972@gmail.com>
2026-02-25 12:22:26 +0545

    vercel.json added to the root

--- src/config/db.js
+++ src/config/db.js
@@ -6,7 +6,9 @@ import { neon, neonConfig } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 import * as  schema from "../models/schema.js";
 
-
+if (!process.env.DATABASE_URL) {
+    throw new Error('DATABASE_URL is not set!'); // will show clearly in Vercel logs
+}
 
 const sql = neon(process.env.DATABASE_URL);
 
