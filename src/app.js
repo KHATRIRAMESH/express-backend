0d721ecee43d66bf585e7fdf2e5079feac9bd830
KHATRIRAMESH <khatriramesh972@gmail.com>
2026-02-25 11:16:12 +0545

    express backend fixed for serverless function

--- src/app.js
+++ src/app.js
@@ -1,15 +1,19 @@
 
 import "dotenv/config.js";
 import express from 'express';
+import cors from 'cors';
 import postsRoute from "./routes/posts.route.js"
 import authRoutes from "./routes/auth.route.js"
 
 const app = express();
 
+const corsOptions = {
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+}
+
+app.use(cors(corsOptions))
 app.use(express.json()); // Middleware to parse JSON bodies
-
-
-
 app.get("/", (req, res) => {
     res.send("Blog Post API - Welcome!");
 });
--- src/config/db.js
+++ src/config/db.js
@@ -2,8 +2,10 @@
 import { setGlobalDispatcher, Agent } from "undici";
 setGlobalDispatcher(new Agent({ connect: { family: 4 } }));
 import "dotenv/config.js";
-import { neon } from '@neondatabase/serverless';
+import { neonConfig, neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
+
+neonConfig.fetchConnectionCache = true;
 
 const sql = neon(process.env.DATABASE_URL);
 
